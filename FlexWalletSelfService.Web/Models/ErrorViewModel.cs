a0ac56f6092b931ae1ab8e6c58d372e61858a08a
dolapo <midtech4info@gmail.com>
2022-10-07 22:51:40 +0100

    Add project files.

---
+++ FlexWalletSelfService.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FlexWalletSelfService.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
